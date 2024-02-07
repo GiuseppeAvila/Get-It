220e75f15345adba59364b82020d8d0f81594b6e
GiuseppeAvila <49525480+GiuseppeAvila@users.noreply.github.com>
2024-02-06 21:16:55 -0300

    Handout Parte 3
    Templates e views

--- utils.py
+++ utils.py
@@ -1,4 +1,10 @@
+import json
+
 def extract_route(request):
     return request.split(' ')[1][1:]
 
 read_file = lambda filepath: open(filepath, 'rb').read()
+
+load_data = lambda filename: json.load(open('data/' + filename))
+
+load_template = lambda filename: open('templates/' + filename).read()
